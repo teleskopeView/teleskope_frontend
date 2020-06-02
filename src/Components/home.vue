92b478ddaccacbd1a2b89e70d7da855ae3ccafab
Efrat19 <efrat890089@gmail.com>
2020-06-02 21:24:53 +0300

    add ns search

--- src/Components/home.vue
+++ src/Components/home.vue
@@ -2,7 +2,7 @@
   <div>
     <page-title :heading="heading" :subheading="subheading" :icon="icon"></page-title>
     <div class="row mt-4">
-      <div class="col-lg-6 col-xl-3" v-for="(namespace,index) in namespaces" :key="index">
+      <div class="col-lg-6 col-xl-3" v-for="(namespace,index) in filteredNamespaces" :key="index">
         <router-link :to="`/namespaces/${namespace}`" style="text-decoration:none; color:inherit;">
           <div class="card mb-3 widget-content">
             <div class="widget-content-wrapper">
@@ -22,7 +22,7 @@
 <script>
 import PageTitle from "../Layout/Components/PageTitle.vue";
 import ApiService from "../Services/apiService";
-
+import { eventBus } from '../EventBus'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faTrashAlt,
@@ -47,11 +47,21 @@ export default {
       subheading: "Select a Namspace to Start",
       icon: "pe-7s-helm icon-gradient bg-tempting-azure",
       apiService: new ApiService(),
-      namespaces: []
+      namespaces: [],
+      filteredNamespaces: []
     };
   },
   async beforeMount() {
     this.namespaces = await this.apiService.listNamespace();
+    this.filteredNamespaces = this.namespaces;
+    eventBus.$on('search',this.onSearch)
+  },
+  methods: {
+    onSearch(keyword) {
+      console.log(keyword);
+      this.filteredNamespaces = this.namespaces.filter(namespace => namespace.includes(keyword))
+    }
   }
+
 };
 </script>
--- src/Layout/Components/Header/SearchBox.vue
+++ src/Layout/Components/Header/SearchBox.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="search-wrapper" v-bind:class="{ active: searchOpen }">
             <div class="input-holder">
-                <input type="text" class="search-input" placeholder="Type to search"/>
+                <input type="text" class="search-input" placeholder="Type to search" @keyup="onChange"/>
                 <button class="search-icon" v-on:click="searchOpen = !searchOpen"><span/></button>
             </div>
             <button class="close" v-on:click="searchOpen = !searchOpen"/>
@@ -11,12 +11,18 @@
 </template>
 
 <script>
+import { eventBus } from '../../../EventBus'
     export default {
         data() {
             return {
                 searchOpen: false,
             }
         },
+        methods: {
+            onChange(evt) {
+                eventBus.$emit('search', evt.target.value);
+            }
+        }
     }
 </script>
 
